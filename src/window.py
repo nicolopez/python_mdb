f099b2afdad301aa2dce4818c47232dab760e83c
Nico Lopez <nicolopez2410@gmail.com>
2016-05-12 09:50:10 -0300

    window modified to be a class

--- src/window.py
+++ src/window.py
@@ -2,18 +2,21 @@
 
 from gi.repository import Gtk
 
+class Pymdb:
+    def __init__(self):
+        window = Gtk.Window()
+        window.set_title('A Python Movie Database')
+        window.set_default_size(1024, 768)
+        window.set_position(Gtk.WindowPosition.CENTER)
+        window.connect('destroy', self.destroy)
+        window.show_all()
+
+    def destroy(self, window):
+        Gtk.main_quit()
+
 def main():
-    window = Gtk.Window()
-    window.set_title('A Python Movie Database')
-    window.set_default_size(1024, 768)
-    window.set_position(Gtk.WindowPosition.CENTER)
-    window.connect('destroy', destroy)
-    window.show_all()
+    app = Pymdb()
     Gtk.main()
-
-def destroy(window):
-    Gtk.main_quit()
-
 
 if __name__ == '__main__':
     main()
